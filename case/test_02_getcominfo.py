3d41e677de2e01fed0bc7352b86be8774980125b
ddddd <test>
2019-08-25 17:52:06 +0800

    修改文件case文件名

---
+++ case/test_02_getcominfo.py
@@ -0,0 +1,9 @@
+# -*- coding:utf-8 -*-
+from public.get_config import Excel_data
+from public.get_testdata import ReadExcel
+from public.apimethod import Apimethod
+import unittest
+import os
+
+class Getcominfo(unittest.TestCase):
+    def setUp(self):
