dd7fa3f64e4d160fbe2f77a663a7f865037695d2
ddddd <test>
2019-08-25 23:40:00 +0800

    更新main

--- main.py
+++ main.py
@@ -1,1 +1,20 @@
 # -*- coding:utf-8 -*-
+import unittest
+import os
+class Runcase(unittest.TestCase):
+    def all_case(self):
+        cur_path = os.path.abspath(os.path.dirname(os.path.dirname(__file__)))
+        case_path = cur_path + 'case'
+        print(case_path)
+        # testcase = unittest.TestSuite()
+        discover = unittest.defaultTestLoader.discover(case_path,pattern="test*.py",top_level_dir=None)
+        runner = unittest.TextTestRunner(verbosity=2)
+        test_unit = unittest.TestSuite()
+        for test_suite in discover:
+            for test_case in test_suite:
+                test_unit.addTest(test_case)
+        runner.run(test_unit)
+        # print(testcase)
+        # return testcase
+if __name__ == '__main__':
+    unittest.main()
