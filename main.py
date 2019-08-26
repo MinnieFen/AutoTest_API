3e9a3de9902b522559de7ee141cd0f9a926c6578
ddddd <ccc>
2019-08-26 16:00:13 +0800

    更新main文件，批量执行测试用例

--- main.py
+++ main.py
@@ -1,20 +1,20 @@
 # -*- coding:utf-8 -*-
 import unittest
 import os
-class Runcase(unittest.TestCase):
-    def all_case(self):
-        cur_path = os.path.abspath(os.path.dirname(os.path.dirname(__file__)))
-        case_path = cur_path + 'case'
-        print(case_path)
-        # testcase = unittest.TestSuite()
-        discover = unittest.defaultTestLoader.discover(case_path,pattern="test*.py",top_level_dir=None)
-        runner = unittest.TextTestRunner(verbosity=2)
-        test_unit = unittest.TestSuite()
-        for test_suite in discover:
-            for test_case in test_suite:
-                test_unit.addTest(test_case)
-        runner.run(test_unit)
-        # print(testcase)
-        # return testcase
+
+def add_case(caseName = 'case',rule = 'test*.py'):
+    cur_path = os.path.dirname(os.path.realpath(__file__))
+    case_path = os.path.join(cur_path,caseName)
+    testcase = unittest.TestSuite()
+    discover = unittest.defaultTestLoader.discover(case_path,pattern=rule,top_level_dir=None)
+    runner = unittest.TextTestRunner()
+    for test_suite in discover:
+        for case in test_suite:
+            testcase.addTest(case)
+    runner.run(testcase)
+    print(testcase)
+    return testcase
+
+
 if __name__ == '__main__':
-    unittest.main()
+    add_case()
